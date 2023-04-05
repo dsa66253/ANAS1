ccfc4148b2eb65c3f9a9863bfaa3e368854a85db
mary <dsa66253@yahoo.com.tw>
2023-04-05 23:46:20 +0800

    0405_9

--- runsh.py
+++ runsh.py
@@ -44,13 +44,13 @@ def brutNas():
             0,
             0
         ],
-        "layer_1_2": [
-            1,
-            0,
-            0,
-            0,
-            0
-        ],
+        # "layer_1_2": [
+        #     1,
+        #     0,
+        #     0,
+        #     0,
+        #     0
+        # ],
         # "layer_2_3": [
         #     1,
         #     0,
@@ -58,7 +58,7 @@ def brutNas():
         #     0,
         #     0
         # ],
-        "layer_2_4": [
+        "layer_1_4": [
             1,
             0,
             0,
@@ -108,7 +108,7 @@ def brutNas():
     "0328_24",
     ]
     # for curExpName in expNameList:
-    curExpName = "0405_8"
+    curExpName = "0405_9"
     desDir = join("./log", curExpName)
     makeDir(desDir)
     makeAllDir()
