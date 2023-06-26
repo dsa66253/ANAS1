9e49597c57837995068824ffe60eedbf985b2810
mary <dsa66253@yahoo.com.tw>
2023-06-26 10:11:54 +0800

    0626

--- runsh.py
+++ runsh.py
@@ -103,7 +103,7 @@ def brutNas():
     "0329_25",
     ]
     # for curExpName in expNameList:
-    curExpName = "0625_8"
+    curExpName = "0626"
     desDir = join("./log", curExpName)
     makeDir(desDir)
     makeAllDir()
